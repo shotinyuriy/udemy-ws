6f88fa0d86609bb91eec1b8d83a1b8d5c4862cff
Yuriy Shotin <yshotin@griddynamics.com>
2019-12-09 05:36:00 -0500

    STEP 22 created a POST /users resource and a error handling when a user is not found by id

--- rest-01/src/main/java/com/udemy/microservice/rest01/user/UserResource.java
+++ rest-01/src/main/java/com/udemy/microservice/rest01/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.udemy.microservice.rest01.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -25,7 +30,26 @@ public class UserResource {
 	@GetMapping("/users/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable Integer id) {
 		User userById = getUserService().findById(id);
+		if (userById == null) {
+			throw new UserNotFoundException("id="+id);
+		}
 		return ResponseEntity.ok(userById);
+	}
+	
+	// POST /users
+	@PostMapping("/users")
+	public ResponseEntity<Object> postUser(@RequestBody User newUser) {
+		User savedUser = getUserService().save(newUser);
+		
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId())
+			.toUri();
+		
+		return ResponseEntity
+				.created(location)
+				.build();
 	}
 	
 	public UserService getUserService() {
