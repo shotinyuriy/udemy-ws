da7350357d4fa9f7b3dfc5a88f38670138fa9e7d
Yuriy Shotin <yshotin@griddynamics.com>
2019-12-19 14:57:45 -0500

    STEP 61 introduced a COnfigurationProperties for Limits Service

--- limits-service/src/main/java/com/udemy/microservice/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/udemy/microservice/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.udemy.microservice.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import com.udemy.microservice.limitsservice.bean.LimitsConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private ServiceConfiguration serviceConfiguration;
+	
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsFromConfigurations() {
-		return new LimitsConfiguration(1000, 1);
+		return new LimitsConfiguration(serviceConfiguration.getMaximum(), serviceConfiguration.getMinimum());
 	}
 }
