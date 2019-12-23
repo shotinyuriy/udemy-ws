97180d944ae692f38ad12d69d6ca5b2704e4282d
Yuriy Shotin <yshotin@griddynamics.com>
2019-12-23 07:00:36 -0500

    STEP 75 created a new entity from ExchangeValue

--- currency-exchange-service/src/main/java/com/udemy/microservice/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/udemy/microservice/currencyexchangeservice/ExchangeValue.java
@@ -2,16 +2,27 @@ package com.udemy.microservice.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ExchangeValue {
 
+	@Id
 	private Long id;
 	
+	@Column(name="currency_from")
 	private String from;
 	
+	@Column(name="currency_to")
 	private String to;
 	
 	private BigDecimal conversionMultiple;
 	
+	@Column(nullable = true)
+	private int port;
+
 	public ExchangeValue() {
 		super();
 	}
@@ -39,6 +50,12 @@ public class ExchangeValue {
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
 	}
-	
-	
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
 }
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100001,'USD','INR',65, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100002,'EUR','INR',75, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100003,'AUD','INR',60, 0);
+
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100004,'USD','RUR',62, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100005,'EUR','RUR',72, 0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values(100006,'AUD','RUR',58, 0);
