3ab77b1cd640a5df85dc262966bb635b24db7a68
Yuriy Shotin <yshotin@griddynamics.com>
2019-12-24 05:35:59 -0500

    STEP 88 Conneced Currency Calculation Service with Eureka Naming Server

--- currency-conversion-service/src/main/java/com/udemy/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/udemy/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.udemy.microservice.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.udemy.microservice.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
