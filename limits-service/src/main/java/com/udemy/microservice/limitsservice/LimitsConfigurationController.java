44c12093c089dc7b6f0a16557a7fc54c90697bd8
Yuriy Shotin <yshotin@griddynamics.com>
2019-12-26 08:14:54 -0500

    STEP 110 Implemeted Fault Tolerance example for Limits Service

--- limits-service/src/main/java/com/udemy/microservice/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/udemy/microservice/limitsservice/LimitsConfigurationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.udemy.microservice.limitsservice.bean.LimitsConfiguration;
 
 @RestController
@@ -15,5 +16,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitsConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitsConfiguration(serviceConfiguration.getMaximum(), serviceConfiguration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitsConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitsConfiguration fallbackRetrieveConfiguration() {
+		return new LimitsConfiguration(999, 9);
 	}
 }
